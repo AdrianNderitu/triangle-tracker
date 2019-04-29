e54ab7245b12ef9b6fc479ba25bcb2b962fa3875
Adrian <adriannderitu1@gmail.com>
2019-04-29 12:41:55 +0300

    add scripts.js modification

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,11 @@
-function isTriangle(a,b,c){
+function isTriangle() {
+  var firstInput=document.getElementsById("firt-input").value;
+  var secondInput=document.getElementById("second-input").value;
+
+
+
   else if (a === 0 || b === 0 || c === 0){
-    return false;
+    alert("Is not a triangle.")
   }
   else if(a+b+c)
 }
